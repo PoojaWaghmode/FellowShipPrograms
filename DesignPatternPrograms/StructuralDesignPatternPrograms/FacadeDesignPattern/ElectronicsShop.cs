9549940db6aeeb5dee3ecc729753dcd7a0e4b622
OWNER <waghmodepooja3@gmail.com>
2019-11-27 19:03:46 +0530

    Update

---
+++ DesignPatternPrograms/StructuralDesignPatternPrograms/FacadeDesignPattern/ElectronicsShop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternPrograms.StructuralDesignPatternPrograms.FacadeDesignPattern
+{
+    public interface ElectronicsShop
+    {
+        public void GetProductName();
+        public void GetProductFeatures();
+
+        public void GetProductPrice();
+
+    }
+}
